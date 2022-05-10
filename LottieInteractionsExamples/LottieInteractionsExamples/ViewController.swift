fd8095ec03ef0b591e23b2cc03e458b8bf2bdb7d
Evandro <evandro.hoffmann@gmail.com>
2022-05-10 10:53:13 -0300

    [CHORE] Added base example project

---
+++ LottieInteractionsExamples/LottieInteractionsExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottieInteractionsExamples
+//
+//  Created by Evandro Harrison Hoffmann on 10/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
